85d85edbd75ff68fd4b763ba1bd35626bd6df723
nhut.ngnn <nhutnmse184534@fpt.edu.vn>
2024-12-15 13:15:04 +0700

    main: get loop for alpha value on training

--- main.py
+++ main.py
@@ -11,21 +11,11 @@ from training.BERT_ECAPA import FlexibleMMSER
 # from training.BERT_VGGish import FlexibleMMSER
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+alpha_values = [0.1, 0.3, 0.5, 0.7, 0.9]
 
-wandb.init(
-    project="FlexibleMMSER", 
-    config={
-        "batch_size": 128,
-        "learning_rate": 0.0001,
-        "num_epochs": 200,
-        "model": "FlexibleMMSER",
-        "dataset": "IEMOCAP",
-    }
-)
-train_metadata = "/kaggle/input/feature/IEMOCAP_BERT_ECAPA_train.pkl"
-val_metadata = "/kaggle/input/feature/IEMOCAP_BERT_ECAPA_val.pkl"
+train_metadata = "/kaggle/input/feature/IEMOCAP_BERT_WAV2VEC_train.pkl"
+val_metadata = "/kaggle/input/feature/IEMOCAP_BERT_WAV2VEC_val.pkl"
 
-# Datasets and Dataloaders
 BATCH_SIZE = 128
 train_dataset = CustomizedDataset(train_metadata)
 val_dataset = CustomizedDataset(val_metadata)
@@ -33,12 +23,28 @@ val_dataset = CustomizedDataset(val_metadata)
 train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 val_dataloader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
-model = FlexibleMMSER(num_classes=4).to(device)
-print_model_parameters(model)
+for alpha in alpha_values:
+    wandb.init(
+        project="FlexibleMMSER-Alpha-Experiment",
+        config={
+            "batch_size": 128,
+            "learning_rate": 0.0001,
+            "num_epochs": 200,
+            "model": "FlexibleMMSER",
+            "dataset": "IEMOCAP",
+            "feature": "BERT-WAV2VEC",
+            "alpha": alpha
+        }
+    )
+    
+    model = FlexibleMMSER(num_classes=4).to(device)
+    model.alpha = alpha  
+    print(f"Training with alpha = {alpha}")
+    print_model_parameters(model)
 
-save_path = "/kaggle/working/ESD_ENG_CMN_BERT_HuBERT.pt"
-train_and_evaluate(
-    model, train_dataloader, val_dataloader, num_epochs=200, save_path=save_path
-)
+    save_path = f"/kaggle/working/IEMOCAP_BERT_WAV2VEC_alpha_{alpha}.pt"
+    train_and_evaluate(
+        model, train_dataloader, val_dataloader, num_epochs=200, save_path=save_path
+    )
 
-wandb.finish()
+    wandb.finish()
