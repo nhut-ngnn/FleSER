298324e370da7c41368307fe361256b16271a316
nhut.ngnn <nhutnmse184534@fpt.edu.vn>
2024-12-21 02:22:00 +0700

    evaluated: upload confusion matrix

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -4,7 +4,7 @@ from training.CustomizedDataset import CustomizedDataset
 from training.BERT_Wav2Vec import FlexibleMMSER
 from ultis import model_prediction, calculate_accuracy
 import csv
-from sklearn.metrics import confusion_matrix, balanced_accuracy_score, accuracy_score
+from sklearn.metrics import confusion_matrix
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -15,7 +15,6 @@ TEST_METADATA_PATH = "C:/Users/admin/Documents/FuzzyMachineLearning/mymodel/feat
 MODEL_BASE_PATH = "model/IEMOCAP_BERT_Wav2Vec_concat_alpha_{}.pt"
 ALPHA_VALUES = [1]
 NUM_CLASSES = 4 
-RESULTS_CSV_PATH = "performance-analysics/IEMOCAP_BERT_Wav2Vec_concat.csv"
 
 def load_model(model_path, num_classes, device):
     """Load the model from the given path and move it to the specified device."""
@@ -27,13 +26,6 @@ def load_model(model_path, num_classes, device):
 def evaluate_model(model, dataloader, metric_fn):
     """Evaluate the model using the specified dataloader and metric function."""
     return model_prediction(model, dataloader, metric_fn)
-
-def save_results_to_csv(results, file_path):
-    """Save evaluation results to a CSV file."""
-    with open(file_path, mode='w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow(["Alpha", "WA", "UA", "WF1", "UF1"])
-        writer.writerows(results)
 
 def main():
     test_dataset = CustomizedDataset(TEST_METADATA_PATH)
@@ -49,14 +41,13 @@ def main():
             print(f"Evaluating model with alpha = {alpha}")
 
             eval_metrics = evaluate_model(model, test_dataloader, calculate_accuracy)
-            eval_wa, eval_ua, eval_wf1, eval_uf1, y_true_ls, y_pred_ls = eval_metrics
+            _, _, _, _, y_true_ls, y_pred_ls = eval_metrics
             
             print(f"Alpha: {alpha}")
             y_true_ls = np.concatenate(y_true_ls, axis=0)
             y_pred_ls = np.concatenate(y_pred_ls, axis=0)
             cm = confusion_matrix(np.array(y_true_ls), np.array(y_pred_ls))
             print(cm)
-            
             
             cmn = (cm.astype('float') / cm.sum(axis=1)[:, np.newaxis])*100
 
@@ -71,9 +62,6 @@ def main():
             plt.show()
         except FileNotFoundError:
             print(f"Model file not found for alpha = {alpha}. Skipping...\n")
-
-    save_results_to_csv(results, RESULTS_CSV_PATH)
-    print(f"Results saved to {RESULTS_CSV_PATH}")
 
 if __name__ == "__main__":
     main()
